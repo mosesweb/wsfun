33a3d79b0aebcae346217796c4b4de4dffb12aa9
Moses J <mosesjohansson@gmail.com>
2023-01-17 12:22:52 +0100

    Login or not logged in

--- sum-client-react/src/App.tsx
+++ sum-client-react/src/App.tsx
@@ -1,8 +1,13 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import './App.css'
-
+import { getAuth, signInWithPopup, FacebookAuthProvider, signOut } from "firebase/auth";
 import * as WebSocket from "websocket"
+import { initializeApp } from 'firebase/app';
 
+class User {
+  displayName: string = "";
+  email: string = "";
+}
 class ChatMessage {
   time: number = 0
   text: string = "";
@@ -19,14 +24,53 @@ class ChatMessage {
 
 function App() {
   const [messageInput, setMessageInput] = useState("")
-  const [userInput, setuserInput] = useState(localStorage.getItem("username"))
+  const [clientUser, setclientUser] = useState<User | null>(null)
   const [clientSocket, setClientSocket] = useState<WebSocket.w3cwebsocket | null>(null)
 
   const [messages, SetMessages] = useState<ChatMessage[]>([]);
   const textarea = useRef<HTMLTextAreaElement>(null);
+  const provider = new FacebookAuthProvider();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCaUQYTcebWzLOj6vVi-6kKkVIKmMPZPmc",
+    authDomain: "mychat-359818.firebaseapp.com",
+    projectId: "mychat-359818",
+    storageBucket: "mychat-359818.appspot.com",
+    messagingSenderId: "664404803242",
+    appId: "1:664404803242:web:3a6ea67f3674a6f4245ad3",
+    measurementId: "G-2WCTT5WJ1E"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+  const logout = () => {
+    console.log("signout")
+    auth.signOut().then((e) => 
+      {
+        setclientUser(null);
+        alert("You're logged out.");
+      });
+  }
 
   useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      console.log(user);
 
+      if (user) {
+        const clientUser = new User();
+    
+        clientUser.displayName = user.displayName ?? "unknown";
+        clientUser.email = user.email ?? "unknown";
+
+        console.log(clientUser);
+        setclientUser(clientUser);
+      }
+      else {
+       
+      }
+    })
+   
     const socket = new WebSocket.w3cwebsocket('wss://myservice-ggddbbhemq-nw.a.run.app/ws');
 
     socket.onopen = function () {
@@ -61,7 +105,7 @@ function App() {
     const msg = JSON.stringify({
       text: messageInput,
       time: +new Date(),
-      user: userInput,
+      user: auth.currentUser?.displayName,
     })
 
     if(messageInput !== "") {
@@ -78,18 +122,52 @@ function App() {
     setMessageInput(event.target.value);
   }
 
-  const handleuserInput = (event: ChangeEvent<HTMLInputElement>)  => {
-    localStorage.setItem("username", event.target.value);
-    setuserInput(event.target.value);
-  }
 
   useEffect(() => {
     console.log(messages);
   }, [messages]);
 
+ const login = () => {
+  signInWithPopup(auth, provider)
+  .then((result: any) => {
+    // The signed-in user info.
+    const user = result.user; 
+    const clientUser = new User();
+
+    clientUser.displayName = user.displayName;
+    clientUser.email = user.email;
+
+    console.log(clientUser);
+    setclientUser(clientUser);
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    if(credential === null) {
+      throw("Err cred null");
+    }
+
+    const accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error: any) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+
+    // ...
+  });
+ }
+
   return (
-    <div className="App">
+   <> <div className="App">
         <p>Chat</p>
+        { auth.currentUser !== null && 
+        <>
         <div className="chatbox">
         {
           messages.map((m, i: number) => {
@@ -101,10 +179,17 @@ function App() {
          <textarea ref={textarea} placeholder="my message" id='msginput' onChange={handleInput}></textarea><br />
          <button  className='sendbtn' onClick={send}>SEND</button>
         </p>
-        <p>
-          <input type="text" placeholder={userInput ?? ""} onChange={handleuserInput} />
-        </p>
-    </div>
+        </>
+        }
+        <div>{clientUser?.displayName ?? ""}</div>
+        {auth.currentUser === null && 
+          <div onClick={() => login()}>Login</div> 
+        }
+          {auth.currentUser !== null && 
+          <div onClick={() => logout()}>Logout {auth.currentUser?.displayName}</div> 
+        }
+    </div> 
+    </>
   )
 }
 
